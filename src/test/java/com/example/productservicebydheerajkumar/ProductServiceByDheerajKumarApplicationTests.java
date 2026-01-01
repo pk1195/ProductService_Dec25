5bd48684d66c6ddacd28b7a805783c43b8262ef5
kedarpoo <168344627+kedarpoo@users.noreply.github.com>
2026-01-01 16:29:39 +0530

    Implemented Fetch type - Eager & Lazy

--- src/test/java/com/example/productservicebydheerajkumar/ProductServiceByDheerajKumarApplicationTests.java
+++ src/test/java/com/example/productservicebydheerajkumar/ProductServiceByDheerajKumarApplicationTests.java
@@ -1,19 +1,27 @@
 package com.example.productservicebydheerajkumar;
 
+import com.example.productservicebydheerajkumar.models.Category;
 import com.example.productservicebydheerajkumar.models.Product;
+import com.example.productservicebydheerajkumar.repository.CategoryRepository;
 import com.example.productservicebydheerajkumar.repository.ProductProjection;
 import com.example.productservicebydheerajkumar.repository.ProductRespository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @SpringBootTest
+@Transactional
 class ProductServiceByDheerajKumarApplicationTests {
 
     @Autowired
     private ProductRespository productRespository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
 
     @Test
     void contextLoads() {
@@ -31,5 +39,18 @@ class ProductServiceByDheerajKumarApplicationTests {
         //projecttion
         List<ProductProjection> allProductsProjection = productRespository.getProductByCategoryIdUsingProjection(1L);
         System.out.println(allProductsProjection.get(0).getId());
+    }
+
+    @Test
+    void fetchTypeTest()
+    {
+        Category cat = categoryRepository.findById(2L).get();
+        System.out.println(cat);
+        System.out.println("We are done here with eager fetch type");
+
+        List<Product> products = cat.getProducts();
+        System.out.println("Size of products is "+products.size());
+
+        System.out.println("We are done here with lazy fetch type");
     }
 }
